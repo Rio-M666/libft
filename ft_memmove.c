3457812fe1d2f30a718fe736b63e1e0b429c9890
Miyachi Rio <mrio@c4r3s13.42tokyo.jp>
2025-04-28 15:52:40 +0900

    first

---
+++ ft_memmove.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 15:34:16 by mrio              #+#    #+#             */
+/*   Updated: 2025/04/28 15:45:22 by mrio             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+    unsigned char *s;
+    int i;
+    
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+    i=0;
+    while(n--)
+    {
+        
+    }
+    
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char buf1[10] = "abcdefghij";
+	char buf2[10] = "1234567890";
+	printf("%s\n", (char *)memmove(buf1, buf2, 3));
+	return (0);
+}
