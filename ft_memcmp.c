e75041cbf54697e3fa2bed8b1ff667c721246334
Miyachi Rio <mrio@c3r6s5.42tokyo.jp>
2025-04-29 17:01:06 +0900

    torima

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 16:48:14 by mrio              #+#    #+#             */
+/*   Updated: 2025/04/29 16:54:26 by mrio             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char *u;
+	unsigned char *c;
+	int i;
+	u = (unsigned char *)s1;
+	c = (unsigned char *)s2;
+	i = 0;
+	while (n--)
+	{
+		if (u[i] != c[i])
+			return (u[i] - c[i]);
+		i++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miyachirio <miyachirio@student.42.fr>      +#+  +:+       +#+        */
+/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 22:46:29 by mrio              #+#    #+#             */
-/*   Updated: 2025/04/29 03:27:23 by miyachirio       ###   ########.fr       */
+/*   Updated: 2025/04/29 16:57:57 by mrio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *str, int c);
-int	ft_isascii(int c);
-
+int		ft_isascii(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_isalnum(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
